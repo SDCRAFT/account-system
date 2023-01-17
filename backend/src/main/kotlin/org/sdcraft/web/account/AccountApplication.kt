38bfe2121366d83395adb64b6f4e608782b4e42e
heartalborada-del <heartalborada@foxmail.com>
2023-01-17 22:58:21 +0800

    立项

---
+++ backend/src/main/kotlin/org/sdcraft/web/account/AccountApplication.kt
@@ -0,0 +1,11 @@
+package org.sdcraft.web.account
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccountApplication>(*args)
+}
---
+++ backend/src/test/kotlin/org/sdcraft/web/account/AccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sdcraft.web.account
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
